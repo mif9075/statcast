eb19196c7594ddd20098e4a886a932aeec46d160
Miguel Fernandez <miguel.fernandez@codeimmersives.com>
2019-04-23 10:50:02 -0400

    Able to access api

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ function init() {
 }
 function sendRequest() {
     const xhr = new XMLHttpRequest();
-    const url = 'https://swapi.co/api/people/25/';
+    const url = 'http://lookup-service-prod.mlb.com/json/named.psc_leader_hit_hr_dist.bam?season=2018&game_type=%27D%27&game_type=%27L%27&game_type%27W%27&game_type=%27F%27&min_hip_count=15';
     xhr.open('GET', url);
     xhr.onload = handleData;
     xhr.send();
 }
 function handleData(event) {
     const person = JSON.parse(event.target.responseText);
-    const {mass, height, name, birth_year} = person;
-    console.log(mass);
-    console.log(height);
+    // const {batter} = person;
+    // console.log(batter);
+    console.log(person.psc_leader_hit_hr_dist.queryResults.row[0].batter)
 }

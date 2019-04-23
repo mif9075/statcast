19da60b5612232b4ee17cba38d8784a8f1f08eb0
Miguel Fernandez <miguel.fernandez@codeimmersives.com>
2019-04-23 12:35:15 -0400

    Completed table, height error

--- main.js
+++ main.js
@@ -19,53 +19,58 @@ function handleData(event) {
     // const {batter} = person;
     // console.log(batter);
     console.log(person.psc_leader_hit_hr_dist.queryResults.row[0].batter);
+    console.log(person.psc_leader_hit_hr_dist.queryResults.row.length);
     // const player = person.psc_leader_hit_hr_dist.queryResults.row[0].batter;
+    
     displayTable(person);
 }
 
 function displayTable(player){
 
     // clearList();
+    for (let i = 0; i < player.psc_leader_hit_hr_dist.queryResults.row.length; i++) {
 
     const batterNumTD = document.querySelector('.batter');
     const batterNumData = document.createElement('p');
-    batterNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[0].batter;
+    batterNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[i].batter;
     batterNumTD.appendChild(batterNumData);
 
     const distanceNumTD = document.querySelector('.distance');
     const distanceNumData = document.createElement('p');
-    distanceNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[0].distance;
+    distanceNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[i].distance;
     distanceNumTD.appendChild(distanceNumData);
 
     const exitNumTD = document.querySelector('.exit');
     const exitNumData = document.createElement('p');
-    exitNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[0].launch_speed;
+    exitNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[i].launch_speed;
     exitNumTD.appendChild(exitNumData);
 
     const launchNumTD = document.querySelector('.launch');
     const launchNumData = document.createElement('p');
-    launchNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[0].launch_angle;
+    launchNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[i].launch_angle;
     launchNumTD.appendChild(launchNumData);
 
     const heightNumTD = document.querySelector('.height');
     const heightNumData = document.createElement('p');
-    heightNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[0].height;
+    heightNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[i].height;
     heightNumTD.appendChild(heightNumData);
 
     const pitcherNumTD = document.querySelector('.pitcher');
     const pitcherNumData = document.createElement('p');
-    pitcherNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[0].pitcher;
+    pitcherNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[i].pitcher;
     pitcherNumTD.appendChild(pitcherNumData);
 
     const pitchNumTD = document.querySelector('.pitch');
     const pitchNumData = document.createElement('p');
-    pitchNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[0].pitch_speed;
+    pitchNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[i].pitch_speed;
     pitchNumTD.appendChild(pitchNumData);
 
     const dateNumTD = document.querySelector('.date');
     const dateNumData = document.createElement('p');
-    dateNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[0].game_id;
+    dateNumData.innerText = player.psc_leader_hit_hr_dist.queryResults.row[i].game_id;
     dateNumTD.appendChild(dateNumData);
+
+    }
 }
 
 function clearList() {
